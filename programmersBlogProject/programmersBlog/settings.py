997efd514eaf3eed66311b086fb9ec4e41d9209a
Agata <amielczarek09@gmail.com>
2021-05-24 15:53:32 +0200

    ordered posts on home page

--- programmersBlogProject/programmersBlog/settings.py
+++ programmersBlogProject/programmersBlog/settings.py
@@ -1,3 +1,6 @@
+import django_heroku
+import dj_database_url
+# from decouple import config ???? 
 """
 Django settings for programmersBlog project.
 
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'programmersBlog.urls'
@@ -80,7 +84,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
